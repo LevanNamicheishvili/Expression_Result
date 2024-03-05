65a47c5c7dfc9d4397d34baccf57216b619cd560
LevanNamicheishvili <117360031+LevanNamicheishvili@users.noreply.github.com>
2024-03-05 13:35:54 -0800

    Create exp.cpp

---
+++ exp.cpp
@@ -0,0 +1,111 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string twoDigitSumer(string a, string b)
+{
+    int i = a.size() - 1;
+    int j = b.size() - 1;
+    int carry = 0;
+    string res = "";
+
+    while (i >= 0 || j >= 0 || carry)
+    {
+        long sum = 0;
+        if (i >= 0)
+        {
+            sum += a[i] - '0';
+            i--;
+        }
+        if (j >= 0)
+        {
+            sum += b[j] - '0';
+            j--;
+        }
+        sum += carry;
+        carry = sum / 10;
+        sum = sum % 10;
+        res = char(sum + '0') + res;
+    }
+
+    return res;
+}
+
+string twoDigitMinuser(string a, string b)
+{
+    int i = a.size() - 1;
+    int j = b.size() - 1;
+    int borrow = 0;
+    string res = "";
+
+    while (i >= 0 || j >= 0)
+    {
+        int digitA = i >= 0 ? a[i] - '0' : 0;
+        int digitB = j >= 0 ? b[j] - '0' : 0;
+
+        if (digitA < digitB + borrow)
+        {
+            digitA += 10;
+            borrow = 1;
+        }
+        else
+        {
+            borrow = 0;
+        }
+
+        res = char(digitA - digitB - borrow + '0') + res;
+
+        i--;
+        j--;
+    }
+
+    while (res.size() > 1 && res[0] == '0')
+    {
+        res.erase(res.begin());
+    }
+
+    return res;
+}
+string twoDigitMultiplier(string num1, string num2)
+{
+    if (num1 == "0" || num2 == "0")
+        return "0";
+
+    string result = "0";
+    int i = num2.size() - 1;
+    while (i >= 0)
+    {
+        string current = "";
+        int carry = 0;
+        int j = num1.size() - 1;
+        while (j >= 0)
+        {
+            int temp = ((num1[j] - '0') * (num2[i] - '0')) + carry;
+            carry = temp / 10;
+            current = char(temp % 10 + '0') + current;
+            j--;
+        }
+        if (carry > 0)
+        {
+            current = char(carry + '0') + current;
+        }
+
+        current += string(num2.size() - i - 1, '0');
+        result = twoDigitSumer(result, current);
+        i--;
+    }
+    return result;
+}
+int main()
+{
+    string n;
+    string n = "21111+23135-1"
+
+        char mult = "*";
+
+    n.find(mult);
+
+    int num = n.find(mult);
+
+    cout << num;
+    return 0;
+}
